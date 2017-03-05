fde8dd71a5697340183bdcd3f3370d6e78de41b8
Randall Spencer <randallspencer@gmail.com>
2017-03-05 13:44:29 -0800

    Add PATCH students route

--- controllers/routes/routes.js
+++ controllers/routes/routes.js
@@ -2,21 +2,47 @@ const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 const bodyParser = require('body-parser');
 const ENV = process.env.NODE_ENV || "development";
-const CONFIG = require("./knexfile.js")[ENV];
+const CONFIG = require("../../knexfile.js")[ENV];
 const KNEX = require('knex')(CONFIG);
+
 ROUTER.use(bodyParser.json());
 ROUTER.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
 
 ROUTER.get('/students', (req, res) => {
-  KNEX('students').then((students) => {
-    res.status(200).json('students')
-  }).catch((err) => {
-    console.error(err);
-    res.status(500);
-  })
+    KNEX('students')
+        .orderBy('name', 'asc')
+        .then((students) => {
+            res.status(200).json(students)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+        });
+});
+
+ROUTER.patch('/students/:id', (req, res) => {
+
+    let id = Number.parseInt(req.params.id);
+
+    if (!id || !size) {
+        res.set('Content-Type', 'text/plain');
+        res.body = 'Bad Request';
+        res.sendStatus(400);
+    }
+
+    let student = KNEX('students')
+        .where('id', id)
+        .update(req.body)
+        .then((data) => {
+            res.sendStatus(202);
+        })
+        .catch((err) => {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        });
 });
 
 module.exports = ROUTER;
