02a4fff4b1d400fa081ba570b1b2bdbe369e04cd
Matt <mmuhr88@gmail.com>
2017-03-05 13:38:09 -0800

    added fullfilled to cohorts/:gnum

--- controllers/routes/routes.js
+++ controllers/routes/routes.js
@@ -21,7 +21,7 @@ ROUTER.get('/students', (req, res) => {
 
 ROUTER.get('/cohorts/:gnum', (req, res) => {
   let gnum = Number.parseInt(req.params.gnum);
-  KNEX('students').innerJoin('cohorts', 'cohorts.id', 'students.cohort_id').where(`gnum`,'=', `${gnum}`).select('name', 'size').then((cohortStudents) => {
+  KNEX('students').innerJoin('cohorts', 'cohorts.id', 'students.cohort_id').where(`gnum`,'=', `${gnum}`).select('name', 'fullfiled', 'size').then((cohortStudents) => {
     res.status(200).json('cohortStudents')
   }).catch((err) => {
     console.error(err);
