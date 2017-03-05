939871854e63aa27527d46b687d7d350065408a6
Matt <mmuhr88@gmail.com>
2017-03-05 13:17:31 -0800

    created cohorts/:gnum route

--- controllers/routes/routes.js
+++ controllers/routes/routes.js
@@ -21,7 +21,12 @@ ROUTER.get('/students', (req, res) => {
 
 ROUTER.get('/cohorts/:gnum', (req, res) => {
   let gnum = Number.parseInt(req.params.gnum);
-  KNEX('students').innerJoin('cohorts', 'cohorts.id', 'students.cohort_id').where(`gnum`,'=', `${gnum}`).select('name', 'size')
-})
+  KNEX('students').innerJoin('cohorts', 'cohorts.id', 'students.cohort_id').where(`gnum`,'=', `${gnum}`).select('name', 'size').then((cohortStudents) => {
+    res.status(200).json('cohortStudents')
+  }).catch((err) => {
+    console.error(err);
+    res.status(500);
+  })
+});
 
 module.exports = ROUTER;
