01c7888a959374686469b8a9ece2f2ac6ed5338a
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-03-06 12:48:29 -0800

    updating knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,6 @@ module.exports = {
   development: {
       client: 'pg',
       connection: 'postgres://localhost/gHoodies'
-  },
-  production: {
-    client: 'pg',
-    connection: {
-      host: 'see heroku',
-      database: 'see heroku',
-      user: 'see heroku',
-      port: '5432',
-      password: 'see heroku',
-      uri: 'see heroku'
-    }
   }
 
 };
