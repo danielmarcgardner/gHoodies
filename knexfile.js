b4105f879e3afcb492d1e258cb7c32f1f39c8b83
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-03-06 16:05:06 -0800

    updating files

--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,6 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    // connection: 'postgres://ypwpihbtcuyvtd:80e2e659fc1d27f875fab62868db2868b9eaa59b69564219f0c9dc4e100edabd@ec2-107-20-141-145.compute-1.amazonaws.com:5432/d78b6dusvq1slk',
-    connection: process.env.DATABASE_URL,
     ssl: true
   }
 
